5189ba2eb8d4fdec1bb5bb8f5b8d78a341a6a8cb
Masi Soranummi <masi.soranummi@tuni.fi>
2022-12-01 09:58:56 +0200

    health

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Backend is listening on PORT ${PORT}`);
 });
+
+app.get("/health", (req, res) => {
+    res.send("OK");
+});
