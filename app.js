eb099fa2860dc676dc00826f3bee549d93c6abbb
Masi Soranummi <masi.soranummi@tuni.fi>
2023-01-01 20:38:25 +0200

    add commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const electricityRouter = require("./routes/electricity");
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/electricity", electricityRouter);
 
 app.get("/health", (req, res) => {
--- index.js
+++ index.js
@@ -5,7 +5,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Backend is listening on PORT ${PORT}`);
 });
-
-app.get("/health", (req, res) => {
-    res.send("OK");
-});
